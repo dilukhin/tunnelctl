4a3df53e8ab9e30845d02c957d6e2d834aa7d724
Dmitry Ilukhin <7656484+dilukhin@users.noreply.github.com>
2026-07-20 23:15:16 +0300

    Подключено автоматическое UAC elevation

--- cmd/tunnelctl/main.go
+++ cmd/tunnelctl/main.go
@@ -5,9 +5,18 @@ import (
 	"os"
 
 	"tunnelctl/internal/app"
+	"tunnelctl/internal/elevation"
 )
 
 func main() {
+	handled, exitCode, err := elevation.MaybeRelaunch(os.Args[1:])
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Ошибка: %v\n", err)
+		os.Exit(1)
+	}
+	if handled {
+		os.Exit(exitCode)
+	}
 	if err := app.Run(os.Args[1:]); err != nil {
 		fmt.Fprintf(os.Stderr, "Ошибка: %v\n", err)
 		os.Exit(1)
